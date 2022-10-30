fd3ef7d6757de68185dfbc421d67f9e1cd15a421
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-10-30 13:14:29 -0300

    add prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <h1>Welcome to React Avançado!</h1>;
+import Head from 'next/head'
+
+export default function Home({ title = 'React Avançado' }) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+      </Head>
+
+      <main>
+        <h1>Welcome to React Avançado!</h1>
+      </main>
+    </div>
+  )
 }
